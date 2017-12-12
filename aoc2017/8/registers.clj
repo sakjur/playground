f8a57e53224492965af9a7aed3c20160c34babf9
Emil Tullstedt <emil.tullstedt@tink.se>
2017-12-12 20:03:52 +0100

    Day 8, first task

---
+++ aoc2017/8/registers.clj
@@ -0,0 +1,48 @@
+(def lineparser [:key :op :val :ifkeyword :compkey :compop :compval])
+(def comparators {"<" <, ">" >, ">=" >=, "<=" <=, "==" ==, "!=" not=})
+
+(defn split-words [s] (clojure.string/split s #" "))
+(defn parse-int [s] (Integer/parseInt s))
+
+(defn val-to-int [line]
+  (assoc
+    (assoc
+      line
+      :val
+      (parse-int (get line :val)))
+    :compval 
+    (parse-int (get line :compval))))
+
+(defn run-compare [opcom lcom rcom]
+  ((get comparators opcom) lcom rcom))
+
+(defn execute-lineop [op origval amount]
+  (if (= op "inc") (+ origval amount) (- origval amount)))
+
+(defn run-line [line state]
+  (let [opkey        (get line :key)
+        keystate     (get state opkey 0)
+        compkey      (get line :compkey)
+        compstate    (get state compkey 0)
+        compres      (run-compare (get line :compop)
+                                  compstate
+                                  (get line :compval))]
+    (if compres
+      (assoc state opkey (execute-lineop (get line :op)
+                                         keystate 
+                                         (get line :val)))
+      state)))
+
+(defn run [state lines]
+  (if (empty? lines) state (recur (run-line (first lines) state) (rest lines))))
+
+(->> "input"
+     slurp
+     clojure.string/split-lines
+     (map split-words)
+     (map #(zipmap lineparser %))
+     (map val-to-int)
+     (run {})
+     (map val)
+     (apply max)
+     println)
