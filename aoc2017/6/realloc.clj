781bd713374901815855c6c3ef40a54b9117fa78
Emil Tullstedt <emil.tullstedt@tink.se>
2017-12-12 19:13:42 +0100

    Cleanup 6

--- aoc2017/6/realloc.clj
+++ aoc2017/6/realloc.clj
@@ -1,32 +1,37 @@
-(defn split-on-tabs [s] (clojure.string/split s #"\t"))
+(defn split-tabs [s] (clojure.string/split s #"\t"))
 (defn parse-int [s] (Integer/parseInt s))
-(defn teeprintln [s] (do (println s) s))
+(defn get-index [x xs]
+  (let [index (.indexOf xs x)]
+    (if (= index -1) nil index)))
 
 (defn dostep [n pos xs]
-  (let [realpos (mod pos (count xs))]
+  (let [i       (mod pos (count xs))
+        newval  (inc (nth xs i))]
     (if
       (zero? n)
       xs
       (recur (dec n)
-             (inc realpos)
-             (assoc xs realpos (inc (nth xs realpos)))))))
+             (inc i)
+             (assoc xs i newval)))))
 
 (defn reallocate [step prev xs]
   (let [maxval (apply max xs)
-        maxpos (.indexOf xs maxval)]
-    (if 
-      (= -1 (.indexOf prev xs))
+        maxpos (get-index maxval xs)]
+    (if
+      (nil? (get-index xs prev))
       (recur (inc step)
              (cons xs prev)
-             (dostep maxval (inc maxpos) (assoc xs maxpos 0)))
-      [step (- step (.indexOf (reverse prev) xs))]
+             (dostep maxval
+                     (inc maxpos)
+                     (assoc xs maxpos 0)))
+      [step (- step (get-index xs (reverse prev)))]
       )
     )
   )
 
 (->> "banks"
      slurp
-     split-on-tabs
+     split-tabs
      (map clojure.string/trim)
      (map parse-int)
      (apply vector)
