33a006d1e3236cf72d7b219e9bd1b3da74278ad0
Emil Tullstedt <emil.tullstedt@tink.se>
2017-12-13 22:42:08 +0100

    12**

--- aoc2017/12/group.clj
+++ aoc2017/12/group.clj
@@ -3,6 +3,7 @@
 (defn split-arrows [s] (clojure.string/split s #"<->"))
 (defn to-int [s] (Integer/parseInt (clojure.string/trim s)))
 (defn difference [a b] (clojure.set/difference a b))
+(defn listdiff [xs diffset] (filter #(contains? diffset %) xs))
 
 (defn split-children' [pid]
   [(to-int (first pid))
@@ -14,11 +15,10 @@
 (defn count-connections [queue visited xs]
   (if
     (empty? queue)
-    (count visited)
+    [(count visited) visited]
     (let [head    (first queue)
           tail    (rest queue)
-          headval (second (first (filter #(= (first %) head) xs)))
-          ]
+          headval (second (first (filter #(= (first %) head) xs)))]
       (recur
       (apply conj
              tail
@@ -27,10 +27,21 @@
       xs
       ))))
 
+(defn total-groups [elem groups xs]
+  (let [group  (second (count-connections #{elem} #{} xs))
+        new-xs (filter #(not (contains? group (first %))) xs)]
+    (if
+      (empty? xs)
+      (count groups)
+      (recur (first (first new-xs)) (cons group groups) new-xs))
+    )
+  )
+
 (->> "input"
      slurp
      clojure.string/split-lines
      (map split-arrows)
      split-children
-     (count-connections #{0} #{})
+     ;(count-connections #{0} #{})
+     (total-groups 0 [])
      println)
